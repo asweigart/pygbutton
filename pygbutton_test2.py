fc06db3e2230fbf4495efa80dff3f02e49bc2202
Al Sweigart <asweigart@gmail.com>
2012-10-22 12:58:29 -0700

    Custom comic sans font on Windows machines. (Change to test 2)

--- pygbutton_test2.py
+++ pygbutton_test2.py
@@ -1,5 +1,6 @@
 import pygame, pygbutton, sys
 from pygame.locals import *
+import platform
 
 FPS = 30
 WINDOWWIDTH = 300
@@ -15,6 +16,9 @@ def main():
 
     buttonHello = pygbutton.PygButton((50, 100, 200, 30), 'Hello')
     buttonToggleVis = pygbutton.PygButton((50, 50, 200, 30), 'Toggle Button Visibility')
+
+    if platform.system() == 'Windows':
+        buttonHello.font = pygame.font.SysFont('comicsansms', 20) # Unfortunately, this line will only work on Windows machines.
 
     visMode = True
 
