0efb5b58c2f631d0a6300976e1974e1d3bc66226
Al Sweigart <asweigart@gmail.com>
2014-10-31 05:40:19 -0700

    Prepping files to upload to PyPI.

---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+.. Pygbutton documentation master file, created by
+   sphinx-quickstart on Fri Oct 31 05:00:37 2014.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Pygbutton's documentation!
+=====================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+
+setup(
+    name='Pygbutton',
+    version='0.9.0',
+    url='https://github.com/asweigart/pygbutton',
+    author='Al Sweigart',
+    author_email='al@inventwithpython.com',
+    description=('A button GUI module for Pygame.'),
+    license='BSD',
+    packages=['pygbutton'],
+    test_suite='tests',
+    install_requires=[],
+    keywords="pygame button gui ui",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Win32 (MS Windows)',
+        'Environment :: X11 Applications',
+        'Environment :: MacOS X',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
